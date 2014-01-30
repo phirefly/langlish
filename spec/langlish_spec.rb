4255c79141ad4303bb53ccf8c7f4e52a97944085
Raphael Villas <raphael.villas@gmail.com>
2014-01-29 23:08:29 -0600

    Added specs #set_sentence

--- spec/langlish_spec.rb
+++ spec/langlish_spec.rb
@@ -1,37 +1,59 @@
-require_relative '../lib/langlish'
+require 'spec_helper'
 
 describe Langlish do
-  let (:dsl) {
-    Langlish.new do
-      set_sentence do
-        "The force is strong with this one."
-      end
+  context 'non-dsl' do
+    let (:dsl) {
+      Langlish.new do
+        set_sentence do
+          "The force is strong with this one."
+        end
 
-      on_beginning do
-        "Alam mo ba"
-      end
+        on_beginning do
+          "Alam mo ba"
+        end
 
-      on_end do
-        "Talaga!"
+        on_end do
+          "Talaga!"
+        end
+      end
+    }
+
+    it 'sets the original sentence' do
+      dsl.instance_of?(Langlish).should be_true
+      dsl.original_sentence.should == "The force is strong with this one."
+    end
+
+    describe '#say_original_sentence' do
+      it 'returns the original sentence' do
+        dsl.say_original_sentence.should == "The force is strong with this one."
       end
     end
-  }
 
-  it 'sets the original sentence' do
-    dsl.instance_of?(Langlish).should be_true
-    dsl.original_sentence.should == "The force is strong with this one."
-  end
-
-  describe '#say_original_sentence' do
-    it 'returns the original sentence' do
-      dsl.say_original_sentence.should == "The force is strong with this one."
+    describe '#say_new_sentence' do
+      it 'returns the new sentence' do
+        dsl.say_new_sentence.should == "Alam mo ba The force is strong with this one. Talaga!"
+      end
     end
   end
 
-  describe '#say_new_sentence' do
-    it 'returns the new sentence' do
-      dsl.say_new_sentence.should == "Alam mo ba The force is strong with this one. Talaga!"
+  context 'dsl' do
+
+    describe '#set_sentence' do
+        let(:langlish) {
+          Langlish.new do
+            set_sentence {"say something"}
+            end
+        }
+
+      it 'sets instance variables' do
+        langlish.instance_variable_get(:@original_sentence).should == 'say something'
+        langlish.instance_variable_get(:@sentence).should == 'say something'
+      end
+
+      it 'uses an existing sentence'
+
     end
   end
+
 end
 
