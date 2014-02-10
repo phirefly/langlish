605514607e0db19bbbf6aeea55bb9184a30529b3
Raphael Villas <raphael.villas@gmail.com>
2014-02-09 23:51:45 -0600

    Updated FlipTalk class

--- lib/flip_talk.rb
+++ lib/flip_talk.rb
@@ -1,25 +1,27 @@
-require 'langlish'
+require File.expand_path('./lib/langlish.rb')
 
 class FlipTalk
-  Langlish.new do
-    set_sentence do
-      "This is a sentence to update. Once there was Little Red Riding Hood. It was the girl who wore red. One day she was on her way to Grandma's house."
+  def initialize
+
+    Langlish.new do
+      set_sentence do
+        "This is a sentence to update. Once there was Little Red Riding Hood. It was the girl who wore red. One day she was on her way to Grandma's house."
+      end
+
+      on_beginning do
+        "Alam mo ba"
+      end
+
+      on_end do
+        "Talaga!"
+      end
+
+      replace_word("the"){ "ano" }
+      replace_word("Grandma"){ "Lola" }
+      end_sentences { "O po" }
+
+      say_original_sentence
+      say_new_sentence
     end
-
-    on_beginning do
-      "Alam mo ba"
-    end
-
-    on_end do
-      "Talaga!"
-    end
-
-    replace_word("the"){ "ano" }
-    replace_word("Grandma"){ "Lola" }
-    end_sentences { "O po" }
-
-    say_original_sentence
-    say_new_sentence
-
   end
 end
