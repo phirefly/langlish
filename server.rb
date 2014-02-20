3f81b34149707f4334866e658529a15863fb88a9
Raphael Villas <raphael.villas@gmail.com>
2014-02-19 23:09:53 -0600

    Stubbed response

--- server.rb
+++ server.rb
@@ -1,12 +1,17 @@
 require 'sinatra'
 require 'haml'
 require 'json'
+require 'pry'
+require File.expand_path('./lib/flip_talk.rb')
 
 get '/' do
   haml :index
 end
 
 get '/translate' do
+  # lang = params[:lang] ||= 'default'
+  flip_talk = FlipTalk.new
+
   content_type :json
-    { :originalText => 'original text', :translatedText => 'translated text' }.to_json
+    { :originalText => 'original sentence', :translatedText => 'translated text'}.to_json
 end
