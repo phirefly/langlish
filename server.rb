25cce724dd84cb413060a11dcaae9c238762719a
Raphael Villas <raphael.villas@gmail.com>
2014-02-22 23:07:10 -0800

    Use langlish in request

--- server.rb
+++ server.rb
@@ -9,8 +9,25 @@ get '/' do
 end
 
 get '/translate' do
-  flip_talk = FlipTalk.new
+  fliptalk = Langlish.new do
+    set_sentence do
+      "This is a sentence to update. Once there was Little Red Riding Hood. It was the girl who wore red. One day she was on her way to Grandma's house."
+    end
+
+    on_beginning do
+      "Alam mo ba"
+    end
+
+    on_end do
+      "Talaga!"
+    end
+
+    replace_word("the"){ "ano" }
+    replace_word("Grandma"){ "Lola" }
+    end_sentences { "O po" }
+
+  end
 
   content_type :json
-    { :originalText => 'original sentence', :translatedText => 'translated text'}.to_json
+    { :originalText => fliptalk.say_original_sentence, :translatedText => fliptalk.say_new_sentence}.to_json
 end
